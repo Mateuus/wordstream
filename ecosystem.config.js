12b5d3ac4e8b824e3df55751a9605f84d4ed274d
Mateuus <mateuus27@outlook.com>
2025-10-10 03:21:21 -0300

    Test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'npm',
       args: 'start',
       cwd: '/home/mateuus/bdj/wordstream',
-      instances: 'max',
+      instances: '2',
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'development',
